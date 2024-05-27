7c08cbb2b4615d9b6a13b8f25cc9e8798b7a2b8f
Mackenzie Whitney <mackenziewhtn@gmail.com>
2024-05-27 09:46:16 -0600

    changed website to use express except for greetings pages

--- app.js
+++ app.js
@@ -12,39 +12,17 @@ console.log(moment().format('MMM Do, YYYY'));
 app.listen(8000, ()=> {
     console.log(`Server is listening on port ${port}. Ready to accept requests`);
 })
-/* http.createServer((request, response) => {
-    console.log("HTTP Request: " + request.url);
-    let parsedAddress = url.parse(request.url, true);
-    let file = "." + parsedAddress.pathname;
 
-    if(parsedAddress.pathname == "/"){
-        file = `./greetingsPages/${greeting.randomGreeting()}.html`;
-    }
-    fs.readFile(file, (err,data) => {
-        console.log("Serving file: "+file);
-        if (err) {
-            console.log(file);
-            console.log("File missing")
-            response.writeHead(404, {"Content-Type": "text/html"});
-            response.write("<h1>404: Page Not Found</h1>");
-            return response.end();
-        }
-        console.log("File present.")
-        var fileParts = file.split(".");
-        var fileType = fileParts[fileParts.length - 1];
-        if (fileType=="css"){
-            response.writeHead(200, { "Content-Type": "text/css"});
-        } else if (fileType == "js") {
-            response.writeHead(200, { "Content-Type": "text/javascript"})
-        } else if (fileType == "html") {
-            response.writeHead(200, { "Content-Type" : "text/html"})
-        } else {
-            response.writeHead(200, { "Content-Type" : "text/plain"})
-        }
-        response.write(data);
-        return response.end();
-    });
-        
-}).listen(port, () => 
-    {console.log(`Server running on port ${port}`)});
- */
+app.use(express.static("views", {
+    extensions: ["html"]
+}));
+app.use(express.static("public"));
+
+app.use((req, res, next) => {
+    res.status(404).send("Sorry, can't find that!");
+})
+
+// can't get this to work.
+app.get('/', function(req, res) {
+    res.render(`/greetingsPages/${greeting.randomGreeting()}.html`);
+  });
